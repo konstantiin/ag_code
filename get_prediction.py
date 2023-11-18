1b1246a5cb84356f14fe7990bcafe90c9d464588
konstantiin <Kostiicc@yandex.ru>
2023-11-18 20:13:13 +0300

    yet another commit

--- get_prediction.py
+++ get_prediction.py
@@ -8,6 +8,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tqdm
 cfg = {}
+eps = 3
+samples_per_day = 8
+days = 3
+with open("illness_info.json", encoding="utf8") as f:
+    illnesses = json.load(f)
+
+grad_coord_x = {}
+grad_coord_y = {}
+
+
+
+
+
+
+
+
 def unpack_matrix(file):#жсон
     with open(file) as f:
         data = np.asarray(json.load(f))
@@ -19,23 +35,21 @@ class IllnessMeter:
         self.ht = highest_temp
         self.lw = lowest_wetness
         self.hw = highest_wetness
-        self.period = per * 6# количество измерений в день
+        self.period = per * 8# количество измерений в день
         self.count = np.zeros((cfg["height"], cfg["width"]), dtype = int)
     def step(self, matrix):
         mat0 = matrix[:, :, 0]
         mat1 = matrix[:, :, 1]
-        m0 = np.ma.masked_where(mat0 <= self.ht, mat0) 
-        m0 = np.logical_and(m0, np.ma.masked_where(mat0 >= self.lt, mat0))
+        m0 = np.where(mat0 <= self.ht + eps, True, False)
+        m0 = np.logical_and(m0, np.where(mat0 >= self.lt - eps, True, False))
 
-        m1 = np.ma.masked_where(mat1 <= self.hw , mat1)
-        m1 = np.logical_and(m1, np.ma.masked_where(mat1 >= self.lw, mat1))
+        m1 = np.where(mat1 <= self.ht + eps, True, False)
+        m1 = np.logical_and(m1, np.where(mat1 >= self.lt - eps, True, False))
         mask = m0 == m1 
         mask = mask == m0
-        self.count = self.count + 1
-        self.count[~mask] = 0
+        self.count[mask] += 1
     def get_result(self):
-
-        return self.count
+        return np.where(self.count >= self.period - eps, 1, 0)
 
 def get_meters(illness_info):
     res = []
@@ -49,25 +63,56 @@ def get_meters(illness_info):
         )
     return res
 
-def get_probability():
-    with open("illness_info.json", encoding="utf8") as f:
-        illnesses = json.load(f)
+def get_probability(mat_paths):
+    
     meters = get_meters(illnesses)
-
-    for i in tqdm.tqdm(range(1)):# iterate over test_samples
-        mat = unpack_matrix(f"days_log/test_sample-{i}.json")# потом изменим на нужное имя
+    for path in tqdm.tqdm(mat_paths):# iterate over test_samples
+        mat = unpack_matrix(path)
         [m.step(mat) for m in meters]
     
+    
     return [m.get_result() for m in meters]
-                  
+
+
+
+
+
+
+def construct_dic(x, y, name):
+    return {"x": grad_coord_x[x], "y": grad_coord_y[y], "name": name}
+
+def save_to_json(name, ans):
+    
+    json_res = []
+    for ill, mat in map(illnesses.keys(), ans):
+        json_res += np.apply_along_axis(lambda x, y: {"x": grad_coord_x[x], "y": grad_coord_y[y], "name": ill},  1,  np.asarray(mat.nonzero()).T).tolist()
+
+    with open(name, "w+") as f:
+        json.dump(json_res)
+
+    
+
+
+#data format: dd.mm.yyyy_tt:tt
+def get_matrix_paths(date_time):
+     return []
+
+
 
 
 if __name__ == "__main__":
+
+    grad_coord_x = get_grad_coord_x()
+    grad_coord_y = get_grad_coord_y()
+
+
     with open("cfg.json") as f:
         cfg = json.load(f)
-
-    ans = get_probability()
-    print(ans[0])
-    plt.matshow(ans[0])
-    plt.show()
+    predict_time = sys.argv[1]
+    paths = get_matrix_paths(predict_time)
+    ans = get_probability(paths) 
+    save_path = f"zalupa_{predict_time}.json"
+    save_to_json(save_path, ans)
+    exit(0)
+    
 
